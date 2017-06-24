df652fcf5e19a503e9806945ea7faadab0fd31c2
Alex Tereschenko <alext.mkrs@gmail.com>
2017-06-24 20:03:49 +0200

    testing sonar automation: add PR- and non-PR scans - final4
    Signed-off-by: Alex Tereschenko <alext.mkrs@gmail.com>

--- sonar-scan.sh
+++ sonar-scan.sh
@@ -5,7 +5,7 @@
 #
 # Author: Alex Tereschenko <alext.mkrs@gmail.com>
 
-sonar_cmd_base="build-wrapper-linux-x86-64 --out-dir ${MRAA_SRC_DIR}/build/bw-output make clear all && \
+sonar_cmd_base="build-wrapper-linux-x86-64 --out-dir ${MRAA_SRC_DIR}/build/bw-output make clean all && \
     sonar-scanner \
         --debug \
         -Dsonar.projectKey=${SONAR_PROJ_KEY} \
@@ -28,9 +28,11 @@ echo ${TRAVIS_REPO_SLUG}
 
 if [ "${TRAVIS_BRANCH}" == "master" -a "${TRAVIS_PULL_REQUEST}" == "false" ]; then
     # master branch push - do a full-blown scan
+    echo "performing master branch push scan"
     sonar_cmd="${sonar_cmd_base}"
 elif [ "${TRAVIS_PULL_REQUEST}" != "false" -a "${TRAVIS_PULL_REQUEST_SLUG}" == "${TRAVIS_REPO_SLUG}" ]; then
     # internal PR - do a preview scan
+    echo "performing internal pull request scan"
     sonar_cmd="${sonar_cmd_base} \
                -Dsonar.analysis.mode=preview \
                -Dsonar.github.pullRequest=${TRAVIS_PULL_REQUEST} \
@@ -38,7 +40,7 @@ elif [ "${TRAVIS_PULL_REQUEST}" != "false" -a "${TRAVIS_PULL_REQUEST_SLUG}" == "
                -Dsonar.github.oauth=${GITHUB_TOKEN} \
                "
 else
-    echo "External pull request or non-master branch push - skipping the scan"
+    echo "Skipping the scan - external pull request or non-master branch push"
     exit 0
 fi
 
@@ -46,19 +48,3 @@ echo "****GOING TO RUN THE SCANNER****"
 echo "${sonar_cmd}"
 
 eval "${sonar_cmd}"
-
-#sonar-scanner \
-#    --debug \
-#    -Dsonar.projectKey=$${SONAR_PROJ_KEY} \
-#    -Dsonar.projectBaseDir=$${MRAA_SRC_DIR} \
-#    -Dsonar.sources=$${MRAA_SRC_DIR} \
-#    -Dsonar.inclusions='api/**/*,CMakeLists.txt,examples/**/*,imraa/**/*,include/**/*,src/**/*,tests/**/*' \
-#    -Dsonar.coverage.exclusions='**/*' \
-#    -Dsonar.cfamily.build-wrapper-output=$${MRAA_SRC_DIR}/build/bw-output \
-#    -Dsonar.host.url=https://sonarqube.com \
-#    -Dsonar.organization=$${SONAR_ORG} \
-#    -Dsonar.login=$${SONAR_TOKEN} \
-#    -Dsonar.analysis.mode=preview \
-#    -Dsonar.github.pullRequest=$${TRAVIS_PULL_REQUEST} \
-#    -Dsonar.github.repository=$${TRAVIS_REPO_SLUG} \
-#    -Dsonar.github.oauth=$${GITHUB_TOKEN}
